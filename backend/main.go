56938f896027761beabdd08e6371bffa1bf2a6c7
carlos.pineda <pinedalberto@hotmail.com>
2025-06-07 13:58:56 -0600

    feat(handler): add contact handler for /contact route

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"portfolio/backend/handler"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/contact", handler.ContactHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
